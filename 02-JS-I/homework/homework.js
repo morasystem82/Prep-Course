7702f72736e51dc1cca2343f8e07e7c2849113e4
morasystem82 <morasystem2016@gmail.com>
2023-01-04 16:25:29 -0400

    tarea homeWork1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,5 +1,4 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'hola';
 
@@ -7,7 +6,7 @@ const nuevaString = 'hola';
 const nuevoNum = 20;
 
 // Crea una variable booleana:
-const nuevoBool = Boolean;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -215,6 +214,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  return nombre + ' ' + apellido;
   
 }
 
@@ -222,12 +222,14 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return 'Hola ' + nombre +'!';
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return (alto*ancho);
   
 }
 
@@ -235,6 +237,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return (lado * 4)
   
 }
 
@@ -242,6 +245,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  return (base * altura)/2;
 
 }
 
@@ -250,7 +254,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro * 1.20;
 }
 
 
@@ -260,11 +264,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+ if (letra.length >1) {
+    return 'Dato incorrecto'
+ } else if (letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u'){
+  return 'Es vocal'
+ } else {
+  return 'Dato incorrecto'
+ }
 }
-
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
